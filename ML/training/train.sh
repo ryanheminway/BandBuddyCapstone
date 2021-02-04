f2bc20f62f0e2169c290480b334968b2b06135ac
malhamb <malham.b@husky.neu.edu>
2021-02-04 16:37:51 -0500

    reverting back to smaller dataset until I can figure out what going on with it. Also fixing script bugs

--- ML/training/train.sh
+++ ML/training/train.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
+if [ -z "$1" ]
+  then
+    echo "Please specify a genre!"
+    exit 1
+fi
 GENRE=$1
 echo $GENRE
 set -x
 mkdir "$GENRE"_model_tmp
 mkdir "$GENRE"_model
-#convert_dir_to_note_sequences --input_dir="$GENRE"_large_midi --output_file="$GENRE".tfrecord --recursive
-music_vae_train --config=groovae_2bar_tap_fixed_velocity --run_dir="$GENRE"_model_tmp --mode=train --examples_path=rock.tfrecord --hparams=batch_size=32, learning_rate=0.0005
+mkdir "$GENRE"_data
+cd groove-v1.0.0-midionly
+find . -name "*$GENRE*" -exec cp "{}" ../"$GENRE"_data \;
+cd ..
+convert_dir_to_note_sequences --input_dir="$GENRE"_data --output_file="$GENRE".tfrecord --recursive
+music_vae_train --config=groovae_2bar_tap_fixed_velocity --run_dir="$GENRE"_model_tmp --mode=train --examples_path="$GENRE".tfrecord --hparams=batch_size=32, learning_rate=0.0005
 cd "$GENRE"_model_tmp
 find . -name "*200000*" -exec cp "{}" ../"$GENRE"_model  \;
 cd ..
@@ -13,3 +22,5 @@ ls
 tar -cvf "$GENRE".tar "$GENRE"_model
 rm -rf "$GENRE"_model_tmp
 rm -rf "$GENRE"_model
+rm -rf "$GENRE"_data
+rm "$GENRE".tfrecord
