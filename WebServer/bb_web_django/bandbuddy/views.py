ebbcab7d4aa48e4ff6f85ed96a2b01899ddf92ab
malhamb <malham.b@husky.neu.edu>
2021-02-23 13:23:22 -0500

    send genre messages to stage 2 with app

--- WebServer/bb_web_django/bandbuddy/views.py
+++ WebServer/bb_web_django/bandbuddy/views.py
@@ -16,13 +16,14 @@ genre_dict= {'rock': 0, "jazz": 1, "funk": 2, "pop": 3, "classical": 4}
 
 def update_genre (request):
     #have it send to stage 2 here
-    print(request.POST.get('genre','0'))
-    host = '127.0.0.1'
-    port = 8080
-    socket_fd = band_buddy_msg.connect_and_register(host, port)
-    
-    #update to send number when avaliable
-    band_buddy_msg.send_midi_data(socket_fd,bytearray([0,1,2,3]),4)
+    genre = request.POST.get('genre','0')
+    if genre != '0':
+        host = '127.0.0.1'
+        port = 8080
+        socket_fd = band_buddy_msg.connect_and_register(host, port, band_buddy_msg.WEB_SERVER_STAGE)
+        print(genre)
+        print(genre_dict[genre])
+        band_buddy_msg.send_webserver_data(socket_fd,genre_dict[genre],band_buddy_msg.STAGE2)
     
     form = GenreForm(request.POST or None) 
 
