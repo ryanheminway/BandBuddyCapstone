6c9c4f64b2e3f3b9d4034b57093f721cbeb7c614
Band Buddy <bandbuddy@bandbuddy.biz>
2021-03-07 04:15:49 +0000

    Added missing function def

--- Firmware/code/network_bb/bb_msg_lib/band_buddy_server.h
+++ Firmware/code/network_bb/bb_msg_lib/band_buddy_server.h
@@ -9,6 +9,7 @@ int register_client(int *client_lst, int id, int sockfd);
 int recieve_stage1_fbb(int &sock_fd, int &payload_sz, uint32_t &wave_data_sz);
 int recieve_stage2_fbb(int &sock_fd, int &payload_sz, uint32_t &midi_data_sz);
 int recieve_header_and_stage2_fbb(int &sockfd, uint32_t &midi_data_sz);
+int recieve_header_and_stage1_fbb(int &sockfd, uint32_t &wav_data_sz);
 int recieve_and_mem_shared_stage2_data(int &sock_fd, int &payload_size);
 int recieve_and_send_webserver_fbb(int &sock_fd, int &payload_size, int &destination_socket);
 int recieve_ack(int &sock_fd, int &stage_id);
