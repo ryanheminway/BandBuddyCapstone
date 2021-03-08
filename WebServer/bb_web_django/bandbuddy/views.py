c303cf5b237bca7993259b7bb85fa7a015e0f45b
malhamb <malham.b@husky.neu.edu>
2021-03-08 13:07:03 -0500

    updateing webserver to have more fields

--- WebServer/bb_web_django/bandbuddy/views.py
+++ WebServer/bb_web_django/bandbuddy/views.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.insert(0, '/home/patch/BandBuddyCapstone/Firmware/code/stage2')
+sys.path.insert(0, '/home/brick/bandbuddy/BandBuddyCapstone/Firmware/code/stage2')
 import band_buddy_msg
 from django.shortcuts import render
 from django.views.generic import TemplateView, CreateView
@@ -16,13 +16,28 @@ genre_dict= {'rock': 0, "jazz": 1, "funk": 2, "pop": 3, "classical": 4}
 def update_genre (request):
     #have it send to stage 2 here
     genre = request.POST.get('genre','0')
+    timbre = request.POST.get('timbre','0')
+    tempo = request.POST.get('tempo','0')
+    temperature = request.POST.get('temperature','0')
+    drums = False
+    guitar = False
+    if request.POST.get('drums','0') == 'on':
+        drums = True
+    if request.POST.get('guitar','0') == 'on':
+        guitar = True
+        
     if genre != '0':
         host = '127.0.0.1'
         port = 8080
-        socket_fd = band_buddy_msg.connect_and_register(host, port, band_buddy_msg.WEB_SERVER_STAGE)    
+        #socket_fd = band_buddy_msg.connect_and_register(host, port, band_buddy_msg.WEB_SERVER_STAGE)    
         print(genre)
-        print(genre_dict[genre])
-        band_buddy_msg.send_webserver_data(socket_fd,genre_dict[genre],band_buddy_msg.STAGE2)
+        print(timbre)
+        print(tempo)
+        print(temperature)
+        print(drums)
+        print(guitar)
+        #print(genre_dict[genre])
+        #band_buddy_msg.send_webserver_data(socket_fd,genre_dict[genre],band_buddy_msg.STAGE2)
 
     form = GenreForm(request.POST or None) 
 
